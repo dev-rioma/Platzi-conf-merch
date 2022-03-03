095be491703a9eab174316761ab166425d86b22f
dev-rioma <dev.rioma@gmail.com>
2022-03-03 16:11:45 -0500

    Add structure Chekout and style

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import '../styles/components/Checkout.css';
 
 function Checkout() {
     return (
-        <h1>Checkout</h1>
+        <div className="Checkout">
+            <div className="Checkout-content">
+                <h3>Lista de Pedidos</h3>
+                <div className="Checkout-item">
+                    <div className="Checkout-element">
+                        <h4>ITEM name</h4>
+                        <span>$18</span>
+                    </div>
+                    <button type='button'>Eliminar</button>
+                </div>
+            </div>
+            <div className="Checkout-sidebar">
+                <h3>Precio Total: $18</h3>
+                <button type='button'>Continuar pedido</button>
+            </div>
+        </div>
     );
 }
 
 export default Checkout;
+
