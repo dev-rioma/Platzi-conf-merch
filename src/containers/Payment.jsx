c3d273296e09843f119fba5536adf20b9c0b0dfc
dev-rioma <dev.rioma@gmail.com>
2022-03-04 15:06:38 -0500

    Add structure and style of Payment and Success

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../styles/components/Payment.css';
 
 function Payment() {
     return (
-        <h1>Payment</h1>
+        <div className="Payment">
+            <div className="Payment-content">
+                <h3>Resumen del pedido</h3>
+                <div className="Payment-button">
+                    Boton de pago con Paypal
+                </div>
+            </div>
+            <div />
+        </div>
     );
 }
 
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import '../styles/components/Success.css'
 
 function Success() {
     return (
-        <h1>Success</h1>
+        <div className="Success">
+            <div className="Success-content">
+                <h2>Jorge, gracias por tu compra</h2>
+                <span>Tu pedido llegara en 3 dias a tu direccion</span>
+                <div className="Success-map">
+                    Gooogle Maps
+                </div>
+            </div>
+        </div>
     );
 }
 
